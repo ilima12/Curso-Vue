3ffe6de50d468b866a0d5f6d24bec57d899b6244
Italo Lima <103470585+ilima12@users.noreply.github.com>
2023-08-16 18:46:50 -0300

    LifeCycle hook

---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LyfeCycle',
+    data() {
+        return{
+            nome: 'Ainda não sei'
+        }
+    },
+    created() {
+        this.nome = 'Matheus'
+    }
+}
+</script>
