2754a4c03d70ab030cf2d02904c029e7c3c8b4a7
Italo Lima <103470585+ilima12@users.noreply.github.com>
2023-08-28 10:18:34 -0300

    Conhecendo as diretivas
    acrescento de v-if, v-else

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,18 +1,25 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias:</p>
     <ul>
     <li>Javascript</li>
     <li>PHP</li>
     <li>Python</li>
    </ul>
+  <p>Mande uma mensagem para: {{ email }}</p>
 </div>
 </template>
 
 <script lang="js">
-
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true
+        }
+    }
 }
 </script>
