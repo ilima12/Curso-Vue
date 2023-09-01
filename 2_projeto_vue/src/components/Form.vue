3fe5253c2386985486daae50ae129cc4774a826d
Italo Lima <103470585+ilima12@users.noreply.github.com>
2023-09-01 14:44:19 -0300

    Update
    uso do css e implementação do methods

--- 2_projeto_vue/src/components/Form.vue
+++ 2_projeto_vue/src/components/Form.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
     <h2>Meu formulário:</h2>
-    <form action="">
+    <form action="" @submit="enviarFormulario($event)">
     <div>
-        <InputText/>
+        <input type="text" v-model="name">
     </div>
     <div>
-        <InputText/>
+        <input type="text" v-model="email"/>
     </div>
     <div>
         <Submit/>
     </div>
     </form>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
 </div>
 </template>
 <script >
@@ -23,6 +26,32 @@ export default {
     components: {
         InputText,
         Submit
+    },
+    data() {
+        return{
+            name: "",
+            email: ""
+        }
+    },
+    methods:{
+        enviarFormulario(e){
+
+            e.preventDefault();
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log("Formulário Enviado!");
+            console.log("O nome é: " + name);
+            console.log("O email é: " + email);
+        }
     }
 }
 </script>
+
+<style scoped>
+p{
+    margin:150px;
+}
+
+</style>
--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -2,11 +2,15 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades!</p>
-        <p>Utilizo as seguintes tecnologias:</p>
-    <ul>
-    <li>Javascript</li>
-    <li>PHP</li>
-    <li>Python</li>
+        <p>Utilizo as seguintes tecnologias para back-end:</p>
+    <ul >
+    <li v-for="(technology, index) in backend_technologies" v-bind:key="index">{{ technology }}</li>
+   </ul>
+   <p>Utilizo as seguintes tecnologias para front-end:</p>
+   <ul>
+    <li v-for="technology in frontend_techologies" :key="technology.id">
+      {{ technology.language }}
+    </li>
    </ul>
    <div>
     <button @click="showEmail">{{ textoBotao }}</button>
@@ -30,7 +34,13 @@ export default {
             mostrar_email: false,
             email: 'italo@gmail.com',
             meu_link: 'https://google.com',
-            textoBotao: 'Mostrar email'
+            textoBotao: 'Mostrar email',
+            backend_technologies: ['Javascript', 'PHP', 'Python'],
+            frontend_techologies: [
+              {id: 1, language: 'HTML'},
+              {id: 2, language: 'CSS'},
+              {id: 3, language: 'Vue'}
+            ]
         }
     },
     methods: {
