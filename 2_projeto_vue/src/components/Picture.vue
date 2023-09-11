3704e6d708200c7f8eb78b0c9a0a4507d7ed6ef5
Italo Lima <103470585+ilima12@users.noreply.github.com>
2023-09-11 17:43:50 -0300

    reutilização de dados
    implementado a função de reutilizar códigos em várias instancias.

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,11 +1,17 @@
 <template>
+<div>
 <img :src="avatar" :alt="descricao">
+<Reutilizacao/>
+
+</div>
 
 </template>
 
 <script>
+import Reutilizacao from './Reutilizacao.vue'
 export default {
     name: 'Picture',
+    components: { Reutilizacao },
     data() {
         return {
            avatar: "/img/avatar.png",
--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -3,14 +3,19 @@
     <h1> Olá Vue! </h1>
     <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
     <MultiplosEventos />
+    <Reutilizacao/>
     </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue';
+import Reutilizacao from 'Reutilizacao.vue';
 export default {
      name: 'PrimeiroComponente',
-     components: { MultiplosEventos },
+     components: { 
+        MultiplosEventos,
+        Reutilizacao 
+    },
      data() {
         return {
             nome: "Matheus",
